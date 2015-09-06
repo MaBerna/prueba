c1deaa0dadd036bc4b04811e6f418c4ce34da02a
MaBerna <maberber2002@yahoo.es>
2015-09-06 22:41:58 +0200

    Update registro.php
    Actualización de los comentarios

--- registro.php
+++ registro.php
@@ -1,15 +1,16 @@
 <?php
-
+//Obtiene los datos del formulario
 $usuario = $_POST["usuario"];
 $correo = $_POST["correo"];
 $contrasenya = $_POST["contrasenya"];
 
-//Enviar correo confirmación
+
 $asunto = "Registro de usuario";
 $mensaje = "Bienvenido te has registrado correctamente . \n" ;
 $mensaje .= "Tu nombre de usuario es: .$usuario . \n" ;
 $mensaje .= " Y tu contraseña es:  . $contrasenya .\n" ;
 
+//Envia correo confirmación
 if(mail($correo, $asunto, $mensaje)){
     
     echo "Correo enviado correctamente";
